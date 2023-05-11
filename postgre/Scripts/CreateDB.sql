328ceac63a41ca30c7702f24c665566d1c90ac82
ggsolaire <luigi.solaro2002@gmail.com>
2023-05-12 00:45:36 +0200

    "Sistemati errori di sintassi"

--- postgre/Scripts/CreateDB.sql
+++ postgre/Scripts/CreateDB.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS utente (
                         username VARCHAR(30) NOT NULL,
                         password VARCHAR(30) NOT NULL,
-                        admin BOOLEAN NOT NULL DEFAULT false
+                        admin BOOLEAN NOT NULL DEFAULT false,
                         CONSTRAINT utente_pk PRIMARY KEY (username)
 );
 
@@ -15,14 +15,14 @@ CREATE TABLE IF NOT EXISTS luogo (
 );
 
 CREATE TABLE IF NOT EXISTS fotografia (
-                        id_foto INTEGER SERIAL NOT NULL,
+                        id_foto SERIAL NOT NULL, --SERIAL indica un BIG INT con sequenza auto gestita dal DB
                         username_autore VARCHAR(30) REFERENCES utente(username) ON DELETE CASCADE,  --CASCADE elimina tutte le foto di quell'utente quando l'utente viene eliminato
                         titolo VARCHAR(30) NOT NULL default 'foto.jpg',
                         dati_foto BYTEA,
                         dispositivo VARCHAR(30) NOT NULL DEFAULT 'Sconosciuto',
                         latitudine FLOAT,
                         longitudine FLOAT,
-                        condivisa BOOLEAN NOT NULL default false
+                        condivisa BOOLEAN NOT NULL default false,
                         CONSTRAINT fotografia_pk PRIMARY KEY (id_foto),
                         CONSTRAINT fotografia_autore_fk FOREIGN KEY (username_autore) REFERENCES utente(username),
                         CONSTRAINT fotografia_luogo_fk FOREIGN KEY (latitudine, longitudine) REFERENCES luogo(latitudine,longitudine)
@@ -41,8 +41,8 @@ CREATE TABLE IF NOT EXISTS collezione (                               --la colle
 CREATE TABLE IF NOT EXISTS contenuto(
                         id_collezione INTEGER NOT NULL,
                         id_foto INTEGER NOT NULL,
-                        CONSTRAINT contenuto_pk PRIMARY KEY (id_collezione, id_foto)
-                        CONSTRAINT contenuto_collezione_fk FOREIGN KEY (id_collezione) REFERENCES collezione(id_collezione) ON DELETE CASCADE
+                        CONSTRAINT contenuto_pk PRIMARY KEY (id_collezione, id_foto),
+                        CONSTRAINT contenuto_collezione_fk FOREIGN KEY (id_collezione) REFERENCES collezione(id_collezione) ON DELETE CASCADE,
                         CONSTRAINT contenuto_fotografia_fk FOREIGN KEY (id_foto) REFERENCES fotografia(id_foto) ON DELETE CASCADE
 );
 
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS frame(
     id_video INTEGER NOT NULL,
     id_foto INTEGER,
     durata INTEGER NOT NULL DEFAULT 0,
-    ordine INTEGER SERIAL NOT NULL DEFAULT 0 --Inserito un Default per avere un valore da cui partire con la funzione di inserimento
+    ordine SERIAL NOT NULL DEFAULT 0, --Inserito un Default per avere un valore da cui partire con la funzione di inserimento
                                              --Così facendo il primo frame avrà ordine 1
     CONSTRAINT frame_pk PRIMARY KEY (id_video),
     CONSTRAINT frame_video_fk FOREIGN KEY (id_video) REFERENCES video(id_video) ON DELETE CASCADE,
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS soggetto (
 CREATE TABLE IF NOT EXISTS tag_soggetto(
     nome_soggetto VARCHAR(30) NOT NULL,
     id_foto INTEGER NOT NULL,
-    CONSTRAINT tag_soggetto_pk (nome_soggetto, id_foto),
-    CONSTRAINT tagsottetto_soggetto_fk (nome_soggetto) REFERENCES soggetto(nome),
-    CONSTRAINT tagsoggetto_fotografia_fk (id_foto) REFERENCES fotografia(id_foto)
+    CONSTRAINT tag_soggetto_pk PRIMARY KEY (nome_soggetto, id_foto),
+    CONSTRAINT tagsottetto_soggetto_fk FOREIGN KEY (nome_soggetto) REFERENCES soggetto(nome),
+    CONSTRAINT tagsoggetto_fotografia_fk FOREIGN KEY (id_foto) REFERENCES fotografia(id_foto)
 );
