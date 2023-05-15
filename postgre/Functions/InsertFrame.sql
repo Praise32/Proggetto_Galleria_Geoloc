514fe23e0afbad3f41703d5620d77bddb52a7e2a
999en <125801405+999en@users.noreply.github.com>
2023-05-15 17:02:49 +0200

    Update InsertFrame.sql
    AGGIUNTO:
    GalleriaUtente
    CollezioniUtente
    ClassificaLuoghi
    
    scrivendo queste funzioni mi sono accorto che magari non può essere chiaro dato che alcune entità hanno il nome della FK diverso dalla PK di riferimento, quindi forse si deve cambiare nome delle fk(?)

--- postgre/Functions/InsertFrame.sql
+++ postgre/Functions/InsertFrame.sql
@@ -1,1 +1,41 @@
+CREATE OR REPLACE FUNCTION GalleriaUtente (Proprietario utente.username%TYPE, Utente utente.username%TYPE) RETURNS SETOF fotografia AS
+$$
+BEGIN
+    RETURN QUERY (
+        SELECT * FROM fotografia
+        WHERE fotografia.username_autore = Proprietario AND (
+            fotografia.username_autore = utente OR fotografia.condivisa
+        )
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION CollezioniUtente(utente utente.username%TYPE) RETURNS SETOF collezione AS
+$$
+BEGIN
+    RETURN QUERY (
+        SELECT * FROM collezione
+        WHERE collezione.username = utente
+    );
+END;
+$$LANGUAGE plpgsql;
+
+CREATE OR REPLACE VIEW ClassificaLuoghi AS
+        SELECT latitudine, longitudine, nome, categoria, COUNT(id_foto) AS NumeroFotografie
+        FROM luogo NATURAL LEFT JOIN fotografia
+        GROUP BY latitudine, longitudine, nome, categoria
+        ORDER BY NumeroFotografie DESC, nome ASC
+        LIMIT 3;
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE OR REPLACE FUNCTION insert_frame_in_video(input_id_video video.id_video%TYPE, input_ordine frame.ordine%TYPE)
