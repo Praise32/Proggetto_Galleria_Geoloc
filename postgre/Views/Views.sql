5bc1723156650e291d2962f4156354806b975464
999en <125801405+999en@users.noreply.github.com>
2023-05-15 22:53:41 +0200

    Update Views.sql
    fotoluogo

--- postgre/Views/Views.sql
+++ postgre/Views/Views.sql
@@ -4,3 +4,11 @@ FROM luogo NATURAL LEFT JOIN fotografia
 GROUP BY latitudine, longitudine, nome, categoria
 ORDER BY NumeroFotografie DESC, nome ASC
 LIMIT 3;
+
+CREATE OR REPLACE VIEW FotoLuogo AS
+SELECT fotografia.id_foto, fotografia.titolo,
+fotografia.dati_foto, fotografia.latitudine, fotografia.longitudine
+luogo.nome, luogo.categoria
+FROM fotografia
+INNER JOIN luogo ON fotografia.latitudine = luogo.latitudine AND 
+fotografia.longitudine = luogo.longitudine;
