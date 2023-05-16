e5c93474bf841da59df8e86c4acedcbc75c24056
ggsolaire <luigi.solaro2002@gmail.com>
2023-05-16 22:23:38 +0200

    Sistemati script in base al nuovo schema del database

--- postgre/Scripts/InsertDB.sql
+++ postgre/Scripts/InsertDB.sql
@@ -2,21 +2,18 @@ INSERT INTO utente (username, password, admin) VALUES
                        ('ggsolaire', 'password', false),
                        ('Cippolean', 'AOO', default),
                        ('Genny', 'IAmVengeance', true);
-                       
-INSERT INTO luogo (latitudine, longitudine, nome, categoria) VALUES 
-                    (12.3375, 45.4341, 'Piazza San Marco', 'Piazza')
-                    (12.4924, 41.8902, 'Colosseo', 'Monumento')
+
+INSERT INTO luogo (latitudine, longitudine, nome, categoria) VALUES
+                    (0, 0, 'Null Island', 'Punto d''interesse'),
+                    (12.3375, 45.4341, 'Piazza San Marco', 'Piazza'),
+                    (12.4924, 41.8902, 'Colosseo', 'Monumento'),
                     (11.2549, 43.7764, 'Biblioteca Nazionale Centrale di Firenze', 'Biblioteca')
 ;
 
-INSERT INTO fotografia (username_proprietario, titolo, dati_foto, dispositivo, condivisa, posizione)
-VALUES ('ggsolaire', 'Festa in giardino', '0x454F46...', 'iPhone X', false, 'Null Island');
+INSERT INTO fotografia (username_autore, titolo, dati_foto, dispositivo, condivisa, latitudine, longitudine)
+VALUES ('ggsolaire', 'Festa in giardino', '0x454F46...', 'iPhone X', false, 0,0),
+       ('Cippolean', 'Panorama', '0x54875A...', true, 12.3375, 45.4341);
 
-INSERT INTO fotografia (username_proprietario, titolo, dati_foto, condivisa, posizione)
-VALUES ('Cippolean', 'Panorama', '0x54875A...', true, 'Piazza San Marco');
-
-INSERT INTO fotografia (username_proprietario, titolo, dati_foto, dispositivo, condivisa)
-VALUES ('Genny', 'Il mio cane', '0x897EBA...', 'Samsung Galaxy', true);
-
-INSERT INTO fotografia (username_proprietario, titolo, dati_foto, dispositivo, condivisa)
-VALUES ('Cippolean', 'Vacanza estiva', '0xA23C5F...', 'Canon EOS', false);
+INSERT INTO fotografia (username_autore, titolo, dati_foto, dispositivo, condivisa)
+VALUES ('Genny', 'Il mio cane', '0x897EBA...', 'Samsung Galaxy', true),
+       ('Cippolean', 'Vacanza estiva', '0xA23C5F...', 'Canon EOS', false);
