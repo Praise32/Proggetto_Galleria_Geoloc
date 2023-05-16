b8104fce1d14dbe544d4a537fea202e337fd7d59
999en <125801405+999en@users.noreply.github.com>
2023-05-17 00:16:21 +0200

    Update Trigger.sql
    AGGIUNTO:
    aggiorna_numero_elementi per collezioni
    Verifica Admin per far sì che non si elimini l'ultimo admin

--- postgre/Trigger/Trigger.sql
+++ postgre/Trigger/Trigger.sql
@@ -12,3 +12,20 @@ CREATE TRIGGER aggiorna_numero_elementi_trigger
 AFTER INSERT ON collezione
 FOR EACH ROW
 EXECUTE FUNCTION aggiorna_numero_elementi();
+
+CREATE OR REPLACE FUNCTION verify_admin()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Verifica che l'utente da eliminare non sia un admin
+    IF OLD.admin = TRUE AND (SELECT COUNT(*) FROM utente WHERE admin = true) = 1 THEN
+        RAISE EXCEPTION 'Non è possibile eliminare l\'unica utente amministratore';
+    END IF;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER verify_admin_trigger
+BEFORE DELETE ON utente
+FOR EACH ROW
+EXECUTE FUNCTION verifica_admin();
