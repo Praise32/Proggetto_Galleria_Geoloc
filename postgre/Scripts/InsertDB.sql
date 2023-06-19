8d9808f4e90ae8b2afc8c892260312aedf29f815
999en <125801405+999en@users.noreply.github.com>
2023-06-19 21:09:40 +0200

    Update InsertDB.sql

--- postgre/Scripts/InsertDB.sql
+++ postgre/Scripts/InsertDB.sql
@@ -1,3 +1,4 @@
+--sono presenti degli insert per testare i vari vincoli e trigger
 --Inserimento degli utenti
 INSERT INTO utente (username, password, admin) VALUES
   ('gennaro', 'password1', true),
@@ -43,26 +44,24 @@ INSERT INTO fotografia (username_autore, dati_foto, dispositivo, data_foto, lati
   ('mariano','11100101', 'Samsung', '2022-12-27 13:00:00', 60.3653, 18.1238, true, 'falzarego.jpg'),
   ('mariano','11100101', 'Samsung', '2022-12-28 16:00:00', 60.3656, 18.7538, true, 'stadio.jpg'),
   ('mariano','11100101', 'Samsung', '2022-12-29 10:00:00', 60.7653, 18.3338, true, 'snowpark.jpg'),
-  ('gennaro', '01010101', 'iPhone', '2023-06-02 01:20:00', 40.3423, 10.9835, true, 'reggia.jpg'),
+  ('gennaro', '01010101', 'iPhone', '2023-06-02 01:20:00', 40.3423, 10.9835, false, 'reggia.jpg'),
   ('luigi', '01010101', 'iPhone', '2023-06-02 01:20:00', 40.3423, 10.9835, true, 'reggia.jpg'),
-  ('mariano', '01110101', 'Samsung', '2023-06-02 01:20:00', 40.3423, 10.9835, true, 'reggia.jpg'),
-  ('gennaro', '01110101', 'iPhone', '2023-06-01 21:20:00', 40.3663, 10.9135, true, 'flora.jpg'),
+  ('mariano', '01110101', 'Samsung', '2023-06-02 01:20:00', 40.3423, 10.9835, false, 'reggia.jpg'),
+  ('gennaro', '01110101', 'iPhone', '2023-06-01 21:20:00', 40.3663, 10.9135, false, 'flora.jpg'),
   ('gennaro', '11010101', 'iPhone', '2023-06-01 21:00:00', 40.3663, 10.9135, true, 'flora.jpg'),
-  ('luigi', '01110101', 'Samsung', '2023-06-01 22:00:00', 40.3663, 10.9634, true, 'comune.jpg'),
+  ('luigi', '01110101', 'Samsung', '2023-06-01 22:00:00', 40.3663, 10.9634, false, 'comune.jpg'),
   ('mariano', '01000101', 'Samsung', '2023-06-01 22:40:00', 40.3934, 10.9217, true, 'viavico.jpg'),
   ('mariano', '01000101', 'Samsung', '2023-06-01 23:20:00', 40.3016, 10.9982, true, 'mcdonald.jpg'),
   ('luigi', '01010001', 'Samsung', '2023-06-01 20:00:00', 40.3486, 10.9779, true, 'pizza.jpg'),
   ('gennaro', '01011101', 'iPhone', '2023-06-01 22:20:00', 40.398, 10.9799, true, 'viaferry.jpg');
-
-
-
-
---Inserimento delle collezioni
+  
+  --Inserimento delle collezioni
 INSERT INTO collezione (id_collezione, username, titolo, data_collezione, numero_elementi) VALUES
   (1, 'gennaro', 'Vacanza a Roma', '2022-06-01 10:00:00', 5),
   (2, 'luigi', 'Viaggio a Parigi', '2023-01-20 11:00:00', 4),
   (3, 'mariano', 'Weekend in montagna', '2022-12-27 12:00:00', 4),
   (4, 'gennaro', 'sabato sera a Caserta', '2023-06-01 20:00:00', 0);
+  
 --Inserimento dei contenuti delle collezioni
 INSERT INTO contenuto (id_collezione, id_foto) VALUES
   (1, 1),
@@ -78,18 +77,23 @@ INSERT INTO contenuto (id_collezione, id_foto) VALUES
   (3, 11),
   (3, 12),
   (3, 13),
-  (4, 14),
   (4, 15),
-  (4, 16),
-  (4, 17),
   (4, 18),
-  (4, 19),
   (4, 20),
   (4, 21),
   (4, 22),
   (4, 23);
-
-
+  
+  /* insert di prova per testare i trigger
+  CASO 1: foto privata ma l'autore è anche il possessore della collezione
+  INSERT INTO contenuto (id_collezione, id_foto) VALUES
+  (4, 14),
+  (4, 17);
+  
+  CASO 2: foto private ma gli autori non sono i possessori della collezione
+  INSERT INTO contenuto (id_collezione, id_foto) VALUES
+  (4, 16),
+  (4, 19);*/
 --Inserimento dei tag degli utenti
 INSERT INTO tag_utente (username, id_foto) VALUES
   ('mariano', 14),
@@ -105,7 +109,7 @@ INSERT INTO tag_utente (username, id_foto) VALUES
   ('mariano', 23),
   ('luigi', 23),
   ('gennaro', 23);
-/*
+
 --Inserimento dei video
 INSERT INTO video (autore, titolo, numero_frames, durata, descrizione) VALUES
   ('gennaro', 'caserta con i miei amici', 0 , 0, 'Video di un sabato sera a caserta con mariano e luigi'),
