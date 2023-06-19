40c6a93d62767e23631821b0362693185ce0d433
999en <125801405+999en@users.noreply.github.com>
2023-06-19 02:48:06 +0200

    Update InsertFrame.sql
    risolta sintassi di foto_per_luogo

--- postgre/Functions/InsertFrame.sql
+++ postgre/Functions/InsertFrame.sql
@@ -33,22 +33,20 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION foto_per_soggetto(in_nome_soggetto soggetto.nome%TYPE, utente_foto fotografia.username_autore%TYPE)
+CREATE OR REPLACE FUNCTION foto_per_luogo(in_nome luogo.nome%TYPE, utente fotografia.username_autore%TYPE)
 RETURNS SETOF fotografia AS
 $$
 BEGIN
     RETURN QUERY (
-    SELECT fotografia.id_foto, fotografia.username_autore, fotografia.titolo, fotografia.dati_foto,
-    fotografia.dispositivo, fotografia.latitudine, fotografia.longitudine, fotografia.condivisa, soggetto.nome
-    FROM fotografia f JOIN tag_soggetto t ON f.id_foto = t.id_foto JOIN soggetto s ON t.nome_soggetto = s.nome
-    WHERE s.nome = in_nome_soggetto AND (
-            fotografia.username_autore = utente_foto OR fotografia.condivisa = TRUE
+        SELECT fotografia.*
+        FROM fotografia
+        JOIN luogo ON fotografia.latitudine = luogo.latitudine AND fotografia.longitudine = luogo.longitudine
+        WHERE luogo.nome = in_nome AND (
+            fotografia.username_autore = utente OR fotografia.condivisa = TRUE
         )
     );
 END;
-$$
-LANGUAGE PLPGSQL;
+$$ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION foto_per_tag_utente(in_username tag_utente.username%TYPE)
