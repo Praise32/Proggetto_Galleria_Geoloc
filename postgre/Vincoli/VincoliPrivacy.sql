fe74b73a8041ecf157739d4eeec40f1c1762673f
Praise32 <98122194+Praise32@users.noreply.github.com>
2023-07-16 16:08:02 +0200

    Update VincoliPrivacy.sql

--- postgre/Vincoli/VincoliPrivacy.sql
+++ postgre/Vincoli/VincoliPrivacy.sql
@@ -49,5 +49,65 @@ $$ LANGUAGE plpgsql;
 
 
 
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Trigger che impedisce ad un'utente di inserire all'interno di Collezioni o Video foto private
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION verifica_privacy_foto() RETURNS TRIGGER AS $$
+BEGIN
+  IF (SELECT condivisa FROM fotografia WHERE NEW.id_foto = fotografia.id_foto) = false THEN
+    RAISE EXCEPTION 'Non è possibile utilizzare foto private per collezioni o video.';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER verifica_privacy_foto_prima_inserimento
+BEFORE INSERT ON contenuto
+FOR EACH ROW EXECUTE PROCEDURE verifica_privacy_foto();
+
+CREATE TRIGGER verifica_privacy_foto_prima_inserimento_frame
+BEFORE INSERT ON frame
+FOR EACH ROW EXECUTE PROCEDURE verifica_privacy_foto();
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Trigger che  setta a NULL le foto in Frame, poichè sta on delete set NULL nella creazione del database e poi elimina la foto dalla collezione
+-- quando passa da condivisa a privata
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION rimuovi_foto_privata() RETURNS TRIGGER AS $$
+BEGIN
+  DELETE FROM contenuto WHERE id_foto = OLD.id_foto;
+  UPDATE frame SET id_foto = NULL WHERE id_foto = OLD.id_foto;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER rimuovi_foto_privata_dopo_update
+AFTER UPDATE OF condivisa ON fotografia
+FOR EACH ROW WHEN (NEW.condivisa = false)
+EXECUTE PROCEDURE rimuovi_foto_privata();
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Trigger che elimina la foto dai video e dalle collezioni quando passa da condivisa a privata
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION rimuovi_foto_privata() RETURNS TRIGGER AS $$
+BEGIN
+  DELETE FROM contenuto WHERE id_foto = OLD.id_foto;
+  DELETE FROM frame WHERE id_foto = OLD.id_foto;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER rimuovi_foto_privata_dopo_update
+AFTER UPDATE OF condivisa ON fotografia
+FOR EACH ROW WHEN (NEW.condivisa = false)
+EXECUTE PROCEDURE rimuovi_foto_privata();
+
 
 
