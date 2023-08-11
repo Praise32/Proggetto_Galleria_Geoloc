7860ab2fc878c89b9fd04044f3c385c015d03022
999en <125801405+999en@users.noreply.github.com>
2023-08-11 14:06:06 +0200

    Create luogo.java

---
+++ src/MODEL/luogo.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Luogo {
+    private float latitudine;
+    private float longitudine;
+    private String nome;
+    private String descrizione;
+
+    public Luogo() {
+    }
+
+    public Luogo(float latitudine, float longitudine, String nome, String descrizione) {
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public float getLatitudine() {
+        return latitudine;
+    }
+
+    public void setLatitudine(float latitudine) {
+        this.latitudine = latitudine;
+    }
+
+    public float getLongitudine() {
+        return longitudine;
+    }
+
+    public void setLongitudine(float longitudine) {
+        this.longitudine = longitudine;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
