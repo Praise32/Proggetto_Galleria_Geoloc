9ac39732b4eb0b93ce364b70c1ca4f94d46cd457
999en <125801405+999en@users.noreply.github.com>
2023-08-11 11:20:59 +0200

    Create Connessione.java
    Connessione DB

---
+++ src/DBconnection/Connessione.java
@@ -0,0 +1,45 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnessioneDatabase {
+
+	// ATTRIBUTI
+	private static ConnessioneDatabase instance = null;
+	public Connection connection = null;
+	private String user = "postgres";
+	private String password = "1234";
+	private String url = "jdbc:postgresql://localhost:5432/NUN M RICORD O NOMME;
+	private String driver = "org.postgresql.Driver";
+
+	// COSTRUTTORE
+	private ConnessioneDatabase() throws SQLException {
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
+	}
+
+
+	public static ConnessioneDatabase getInstance() throws SQLException {
+		if (instance == null) {
+			instance = new ConnessioneDatabase();
+		} else if (instance.connection.isClosed()) {
+			instance = new ConnessioneDatabase();
+		}
+		return instance;
+	}
+	
+	
+}
