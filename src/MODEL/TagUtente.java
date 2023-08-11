95b02276801aab019205e87ba8e0f7b6d10a9793
999en <125801405+999en@users.noreply.github.com>
2023-08-11 17:24:09 +0200

    Update TagUtente.java

--- src/MODEL/TagUtente.java
+++ src/MODEL/TagUtente.java
@@ -1,17 +1,22 @@
 package model;
 
+// Definizione della classe TagUtente nel package model
 public class TagUtente {
-    private Utente username;
-    private Fotografia idFoto;
+    // Attributi privati della classe TagUtente
+    private Utente username;     // Rappresenta l'utente associato al tag
+    private Fotografia idFoto;   // Rappresenta la fotografia associata al tag
 
+    // Costruttore vuoto di default
     public TagUtente() {
     }
 
+    // Costruttore con parametri per inizializzare gli attributi della classe TagUtente
     public TagUtente(Utente username, Fotografia idFoto) {
-        this.username = username;
-        this.idFoto = idFoto;
+        this.username = username;  
+        this.idFoto = idFoto;       
     }
 
+    // Metodi getter e setter per l'attributo username di tipo Utente
     public Utente getUsername() {
         return username;
     }
@@ -20,6 +25,7 @@ public class TagUtente {
         this.username = username;
     }
 
+    // Metodi getter e setter per l'attributo idFoto di tipo Fotografia
     public Fotografia getIdFoto() {
         return idFoto;
     }
