c996083d8e14191b5dee530057ae2b67f9943d9c
999en <125801405+999en@users.noreply.github.com>
2023-08-16 18:11:16 +0200

    Update utenteDAO.java

--- src/DAO/utenteDAO.java
+++ src/DAO/utenteDAO.java
@@ -4,24 +4,24 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
-// Interfaccia UtenteDAO per le operazioni di accesso ai dati dell'entità Utente
+// Questa è l'interfaccia per il DAO (Data Access Object) relativo agli utenti.
 public interface UtenteDAO {
 
-    // Metodo per aggiungere un nuovo utente al database
-    // Parametri:
-    // - username: il nome utente da aggiungere
-    // - password: la password associata all'utente
-    // - admin: indica se l'utente è un amministratore o meno
-    // Restituisce: true se l'utente è stato aggiunto con successo, false altrimenti
-    boolean aggiungiUtenteDAO(String username, String password, boolean admin) throws SQLException;
+    // Metodo per aggiungere un nuovo utente al sistema.
+    public boolean aggiungiUtenteDAO(String username, String password, boolean admin) throws SQLException;
 
-    // Metodo per eliminare un utente dal database
-    // Parametro:
-    // - utenteSelezionato: l'username dell'utente da eliminare
-    // Restituisce: true se l'utente è stato eliminato con successo, false altrimenti
-    boolean eliminaUtenteDAO(String utenteSelezionato) throws SQLException;
+    // Metodo per eliminare un utente dal sistema.
+    public boolean eliminaUtenteDAO(String utenteSelezionato) throws SQLException;
 
-    // ... altri metodi per operazioni CRUD specifiche ...
+    // Metodo per ottenere le collezioni associate a un utente.
+    public boolean VediCollezioniPerUtenteDAO(String usernameSelezionato, ArrayList<int> idCollezioneAssociato) throws SQLException;
 
+    // Metodo per ottenere le fotografie associate a un utente.
+    public boolean VediFotografiaPerUtenteDAO(String usernameSelezionato, ArrayList<int> idFotoAssociato) throws SQLException;
+
+    // Metodo per ottenere i video associati a un utente.
+    public boolean VediVideoPerUtenteDAO(String usernameSelezionato, ArrayList<int> idVideoAssociato) throws SQLException;
+
+    // Metodo per modificare le informazioni di un utente esistente.
+    public boolean modificaUtenteDAO(String usernameSelezionato, String password, boolean admin) throws SQLException;
 }
-
