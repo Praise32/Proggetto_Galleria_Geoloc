92f198b1a29635980f1b3a6696df92496d1de537
999en <125801405+999en@users.noreply.github.com>
2023-08-17 11:29:25 +0200

    Update CollezioneDAO.java
    Dovrebbe essere corretto, aggiunto anche commenti

--- src/DAO/CollezioneDAO.java
+++ src/DAO/CollezioneDAO.java
@@ -1,28 +1,28 @@
-package DAO;
+package DAO;  // Questo codice è all'interno del package "DAO".
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.TimeStamp;
+import java.util.Timestamp;
 
 public interface CollezioneDAO {
+    
+    // Questa interfaccia definisce metodi per l'accesso ai dati delle collezioni.
 
-
-
-
-
-
-
+    // Metodo per aggiungere una nuova collezione al database.
     public boolean aggiungiCollezioneDAO(int idCollezione, String username, String titolo, Timestamp dataCollezione, int numeroElementi) throws SQLException;
 
-
+    // Metodo per eliminare una collezione dal database.
     public boolean eliminaCollezioneDAO(int collezioneSelezionato) throws SQLException;
 
+    // Metodo per aggiungere contenuti (foto) a una collezione esistente.
     public boolean aggiungiContenutoDAO(int idCollezioneSelezionato, ArrayString<int> idFotoSelezionata) throws SQLException;
 
+    // Metodo per eliminare contenuti (foto) da una collezione esistente.
     public boolean eliminaContenutoDAO(int idCollezioneSelezionato, ArrayString<int> idFotoSelezionata) throws SQLException;
 
+    // Metodo per visualizzare i contenuti (foto associate) di una collezione.
     public boolean vediContenutoDAO(int idCollezioneSelezionato, ArrayString<int> idFotoAssociate) throws SQLException;
 
-
+    // Metodo per modificare i dettagli di una collezione esistente nel database.
     public boolean modificaCollezioneDAO(int idCollezione, String username, String titolo, Timestamp dataCollezione, int numeroElementi) throws SQLException;
 }
