9c460557b6fe00c92fafea9c1475f57eac996747
999en <125801405+999en@users.noreply.github.com>
2023-08-17 19:03:35 +0200

    Update Fotografia.java

--- src/MODEL/Fotografia.java
+++ src/MODEL/Fotografia.java
@@ -8,13 +8,20 @@ public class Fotografia {
     private byte[] datiFoto;
     private String dispositivo;
     private Timestamp dataFoto;
-    private Luogo latitudine;
-    private Luogo longitudine;
+    private Luogo luogolat;
+    private Luogo luogolon;
     private boolean condivisa;
     private String titolo;
 
+    //il seguente attributo considera una lista di collezioni in cui è contenuta una fotografia
+    private List<Collezione> listaContenutoFotografia = new ArrayList<>();
     public Fotografia() {
     }
+    //il seguente attributo considera una lista di utenti taggati una fotografia
+    private List<Utente> listaTagUtenteFotografia = newArrayList<>();
+
+    //il seguente attributo considera una lista di utenti taggati una fotografia
+    private List<Soggetto> listaTagSoggetto = newArrayList<>();
 
     public Fotografia(int idFoto, Utente usernameAutore, byte[] datiFoto, String dispositivo, Timestamp dataFoto,
                       Luogo latitudine, Luogo longitudine, boolean condivisa, String titolo) {
@@ -29,6 +36,14 @@ public class Fotografia {
         this.titolo = titolo;
     }
 
+    public void aggiungiCollezione(Collezione col) {
+        this.listaContenutoFotografia.add(col);
+    }
+
+    public void aggiungiTagUtente(Utente usr) {this.listaTagUtenteFotografia.add(usr)};
+
+    public void aggiungiTagSoggetto(Soggetto sog) {this.listaTagSoggetto.add(sog)};
+
     public int getIdFoto() {
         return idFoto;
     }
@@ -40,6 +55,26 @@ public class Fotografia {
     public Utente getUsernameAutore() {
         return usernameAutore;
     }
+
+    public string getUsername(){return Utente.getUsername;}
+
+    public void setUsername(Utente utente){this.utente = utente;}
+
+    public Luogo getLuogolat() {
+        return luogolat;
+    }
+
+    public Luogo getLuogolon() {
+        return luogolon;
+    }
+
+    public float getLatitudine(){return luogolat.getLatitudine;}
+
+    public void setLatitudine(Luogo luogolat){this.luogolat = luogolat;}
+
+    public float getLongitudine(){return Luogo.getLongitudine;}
+
+    public void setLongitudine(Luogo luogolon){this.luogo = luogolon;}
 
     public void setUsernameAutore(Utente usernameAutore) {
         this.usernameAutore = usernameAutore;
@@ -69,21 +104,7 @@ public class Fotografia {
         this.dataFoto = dataFoto;
     }
 
-    public Luogo getLatitudine() {
-        return latitudine;
-    }
 
-    public void setLatitudine(Luogo latitudine) {
-        this.latitudine = latitudine;
-    }
-
-    public Luogo getLongitudine() {
-        return longitudine;
-    }
-
-    public void setLongitudine(Luogo longitudine) {
-        this.longitudine = longitudine;
-    }
 
     public boolean isCondivisa() {
         return condivisa;
@@ -99,5 +120,29 @@ public class Fotografia {
 
     public void setTitolo(String titolo) {
         this.titolo = titolo;
+    }
+
+    public List<Collezione> getListaContenutoFotografia() {
+        return listaContenutoFotografia;
+    }
+
+    public void setListaContenutoFotografia(List<Collezione> listaContenutoFotografia) {
+        this.listaContenutoFotografia = listaContenutoFotografia;
+    }
+
+    public List<Utente> getListaTagUtenteFotografia() {
+        return listaTagUtenteFotografia;
+    }
+
+    public void setListaTagUtenteFotografia(List<Utente> listaTagUtenteFotografia) {
+        this.listaTagUtenteFotografia = listaTagUtenteFotografia;
+    }
+
+    public List<Soggetto> getListaTagSoggetto() {
+        return listaTagSoggetto;
+    }
+
+    public void setListaTagSoggetto(List<Soggetto> listaTagSoggetto) {
+        this.listaTagSoggetto = listaTagSoggetto;
     }
 }
