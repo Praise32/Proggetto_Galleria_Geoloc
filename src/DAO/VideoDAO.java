249b411fccaebeb80695a69122c86ca8f3610997
999en <125801405+999en@users.noreply.github.com>
2023-08-21 17:37:30 +0200

    Update VideoDAO.java
    sbagliata commit ecco VideoDAO

--- src/DAO/VideoDAO.java
+++ src/DAO/VideoDAO.java
@@ -3,18 +3,18 @@ package DAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-// Interfaccia per definire operazioni su soggetti associati a foto
-public interface SoggettoDAO {
+// Interfaccia per definire operazioni su video
+public interface VideoDAO {
 
-    // Aggiunge un nuovo soggetto con il nome e la categoria specificati
-    boolean aggiungiSoggettoDAO(String nome, String categoria) throws SQLException;
+    // Aggiunge un nuovo video con le informazioni specificate
+    boolean aggiungiVideoDAO(int idVideo, String autore, String titolo, String descrizione) throws SQLException;
 
-    // Elimina un soggetto in base al nome specificato
-    boolean eliminaSoggettoDAO(String nomeSelezionato) throws SQLException;
+    // Elimina un video in base all'ID specificato
+    boolean eliminaVideoDAO(String idVideoSelezionato) throws SQLException;
 
-    // Modifica la categoria di un soggetto in base al nome specificato
-    boolean modificaSoggettoDAO(String nomeSelezionato, String categoria) throws SQLException;
+    // Modifica le informazioni di un video in base all'ID specificato
+    boolean modificaVideoDAO(String idVideoSelezionato, String titolo, String descrizione) throws SQLException;
 
-    // Ottiene l'elenco delle foto associate a un soggetto specifico
-    boolean vediFotoAssociate(String nomeSelezionato, ArrayList<Integer> fotografiaAssociato) throws SQLException;
+    // Ottiene l'elenco dei frame associati a un video specifico
+    boolean vediFrameVideoDAO(String idVideoSelezionato, ArrayList<Integer> frameAssociati) throws SQLException;
 }
