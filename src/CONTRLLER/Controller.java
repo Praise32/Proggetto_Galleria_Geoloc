d3cb31604469e5b7918a9657dd28526ea352a820
999en <125801405+999en@users.noreply.github.com>
2023-08-27 18:35:58 +0200

    Update Controller.java
    aggiunte funzioni per utente

--- src/CONTRLLER/Controller.java
+++ src/CONTRLLER/Controller.java
@@ -3,7 +3,7 @@ import DAO.*;
 import ImplementazionePostgresDAO;
 import MODEL.*;
 
-import java.sql.Timestamo;
+import java.sql.Timestamp;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -182,11 +182,216 @@ public class Controller {
         }
     }
 
+    public void dumpDatiVideo {
+        ArrayList<Integer> idVideoList = new ArrayList<>();
+        ArrayList<String> utenteAutoreList = new ArrayList<>();
+        ArrayList<String> titoloList = new ArrayList<>();
+        ArrayList<Integer> numeroFramesList = new ArrayList<>();
+        ArrayList<Integer> durataList = new ArrayList<>();
+        ArrayList<String> descrizioneList = new ArrayList<>();
+
+        galleriaDAO.getListVideoDAO(idVideoList, utenteAutoreList, titoloList, numeroFramesList, durataList, descrizioneList);
+
+        for (int i = 0; i < idVideoList.size(); i++) {
+            Utente utente = null;
+            for (Utente usr : listaUtente) {
+                if( usr.getUtente().equals(utenteAutoreList.get(i))) {
+                    utente = usr;
+                    break;
+                }
+            }
+            Video v = new Video(idVideoList.get(i), autore, titoloList.get(i), numeroFramesList.get(i), durataList.get(i), descrizioneList.get(i));
+            listaVideo.add(v);
+        }
+    }
+
+//____________________________________________________________________________________________________________________//
+//____________________________________________________________________________________________________________________//
 
 
 
 
+//_______________________________________FUNZIONI PER UTENTE//
+    /**
+     * Aggiunge un nuovo utente al sistema.
+     *
+     * @param username   Il nome utente dell'utente.
+     * @param password   La password dell'utente.
+     * @param admin      True se l'utente è un amministratore, False altrimenti.
+     * @throws SQLException   Eccezione sollevata in caso di problemi con il database.
+     */
+    public void aggiungiUtente(String username, String password, boolean admin) throws SQLException {
+        Utente usr = new UtentePostgresDAO();
 
+        boolean control = usr.aggiungiUtenteDAO(username, password, admin);
+
+        if (control) {
+            System.out.println("Utente aggiunto con successo!");
+            Utente u = new Utente(username, passworda, admin);
+            listaUtente.add(a);
+        }
+    }
+
+    /**
+     * Elimina un utente dal sistema in base all'utente selezionato.
+     *
+     * @param utenteSelezionato   Il nome utente dell'utente da eliminare.
+     * @throws SQLException      Eccezione sollevata in caso di problemi con il database.
+     */
+    public void eliminaUtente(String utenteSelezionato) throws SQLException {
+        UtenteDAO u = new UtentePostgresDAO();
+
+        boolean control = u.eliminaUtenteDAO(utenteSelezionato);
+
+        if (control) {
+            System.out.println("Eliminazione avvenuta con successo ...!");
+
+            //SE L'ELIMINAZIONE HA AVUTO SUCCESSO ALLORA ELIMINO ANCHE DAL MODEL L'UTENTE...
+            for (Utente usr : listaUtente) {
+                if (utenteSelezionato.equals(usr.getUsername())) {
+                    listaUtente.remove(usr);
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Impossibile rimuovere l'utente...");
+        }
+    }
+
+    /**
+     * Modifica le informazioni di un utente esistente nel sistema.
+     *
+     * @param usernameSelezionato   Il nome utente dell'utente da modificare.
+     * @param password              La nuova password dell'utente.
+     * @param admin                 True se l'utente deve essere impostato come amministratore, False altrimenti.
+     * @throws SQLException         Eccezione sollevata in caso di problemi con il database.
+     */
+    public void modificaUtente(String usernameSelezionato, String password, boolean admin) throws SQLException {
+
+        UtenteDAO u = new modificaUtenteDAO();
+
+        boolean control = u.modificaUtenteDAO(usernameSelezionato, password, admin);
+
+
+        //se modifico l'utente nel database allora lo modifico anche nel MODEL
+        if (control) {
+            for (Utente usr : listaUtente)
+                if (usr.getUsername().equals(usernameSelezionato)) {
+                    usr.setPassword(password);
+                    usr.setAdmin(admin);
+                    break;
+                }
+        }
+
+    }
+
+    /**
+     * Ottiene una lista di identificatori di collezioni associate a un utente per visualizzazione.
+     *
+     * @param usernameSelezionato   Il nome utente dell'utente per cui si vogliono ottenere gli identificatori delle collezioni.
+     * @return                      Un ArrayList contenente gli identificatori delle collezioni associate all'utente.
+     */
+    public ArrayList<Integer> VediCollezioniPerUtente(String usernameSelezionato) {
+        UtenteDAO u = new UtentePostgresDAO();
+
+        //trovo l'utente a cui si riferisce la vista
+        Utente displayUsr = null;
+        for (Utente usr : listaUtente)
+            if (usr.getUsername().equals(usernameSelezionato)) {
+                displayUsr = usr;
+                break;
+            }
+
+
+        //trovo le collezioni associate
+        ArrayList<Integer> idCollezioneAssociato = new ArrayList<>();
+        boolean control = u.VediCollezioniPerUtenteDAO(usernameSelezionato, idCollezioneAssociato);
+
+        //inzializzo la lista di collezioni a cui afferisce l'utente
+        if (control) {
+            for (Collezione col : listaCollezione)
+                for (int c : idCollezioneAssociato)
+                    if (col.getIdCollezione().equals(c)) {
+                        assert displayUsr != null;
+                        displayUsr.aggiungiCollezione(col);
+                    }
+        }
+
+        return idCollezioneAssociato;
+
+    }
+
+    /**
+     * Ottiene una lista di identificatori di fotografie associate a un utente per visualizzazione.
+     *
+     * @param usernameSelezionato   Il nome utente dell'utente per cui si vogliono ottenere gli identificatori delle fotografie.
+     * @return                      Un ArrayList contenente gli identificatori delle fotografie associate all'utente.
+     */
+    public ArrayList<Integer> VediFotografiaPerUtente(String usernameSelezionato) {
+        UtenteDAO u = new UtentePostgresDAO();
+
+        //trovo l'utente a cui si riferisce la vista
+        Utente displayUsr = null;
+        for (Utente usr : listaUtente)
+            if (usr.getUsername().equals(usernameSelezionato)) {
+                displayUsr = usr;
+                break;
+            }
+
+
+        //trovo le fotografie associate
+        ArrayList<Integer> idFotoAssociato = new ArrayList<>();
+        boolean control = u.VediFotografiaPerUtenteDAO(usernameSelezionato, idFotoAssociato);
+
+        //inzializzo la lista di fotografie a cui afferisce l'utente
+        if (control) {
+            for (Fotografia fot : listaFotografia)
+                for (int f : idFotoAssociato)
+                    if (fot.getIdFoto().equals(f)) {
+                        assert displayUsr != null;
+                        displayUsr.aggiungiFotografia(fot);
+                    }
+        }
+
+        return idFotoAssociato;
+
+    }
+    
+    /**
+     * Ottiene una lista di identificatori di fotografie associate a un utente per visualizzazione.
+     *
+     * @param usernameSelezionato   Il nome utente dell'utente per cui si vogliono ottenere gli identificatori delle fotografie.
+     * @return                      Un ArrayList contenente gli identificatori delle fotografie associate all'utente.
+     */
+    public ArrayList<Integer> VediVideoPerUtente(String usernameSelezionato) {
+        UtenteDAO u = new UtentePostgresDAO();
+
+        //trovo l'utente a cui si riferisce la vista
+        Utente displayUsr = null;
+        for (Utente usr : listaUtente)
+            if (usr.getUsername().equals(usernameSelezionato)) {
+                displayUsr = usr;
+                break;
+            }
+
+
+        //trovo i video associati
+        ArrayList<Integer> idVideoAssociato = new ArrayList<>();
+        boolean control = u.VediVideoPerUtenteDAO(usernameSelezionato, idVideoAssociato);
+
+        //inzializzo la lista di video a cui afferisce l'utente
+        if (control) {
+            for (Video vid : listaVideo)
+                for (int v : idVideoAssociato)
+                    if (vid.getIdVideo().equals(v)) {
+                        assert displayUsr != null;
+                        displayUsr.aggiungiVideo(vid);
+                    }
+        }
+
+        return idVideoAssociato;
+
+    }
 
 
 
