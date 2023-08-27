dc6e392f26a961da7f86f4a31d8929b6698c1106
999en <125801405+999en@users.noreply.github.com>
2023-08-27 18:05:30 +0200

    Update UtenteDAO.java
    modificato type arraylist

--- src/DAO/UtenteDAO.java
+++ src/DAO/UtenteDAO.java
@@ -2,7 +2,7 @@ package DAO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Timestamp;
 
 // Questa è l'interfaccia per il DAO (Data Access Object) relativo agli utenti.
 public interface UtenteDAO {
@@ -14,13 +14,13 @@ public interface UtenteDAO {
     public boolean eliminaUtenteDAO(String utenteSelezionato) throws SQLException;
 
     // Metodo per ottenere le collezioni associate a un utente.
-    public boolean VediCollezioniPerUtenteDAO(String usernameSelezionato, ArrayList<int> idCollezioneAssociato) throws SQLException;
+    public boolean VediCollezioniPerUtenteDAO(String usernameSelezionato, ArrayList<Integer> idCollezioneAssociato) throws SQLException;
 
     // Metodo per ottenere le fotografie associate a un utente.
-    public boolean VediFotografiaPerUtenteDAO(String usernameSelezionato, ArrayList<int> idFotoAssociato) throws SQLException;
+    public boolean VediFotografiaPerUtenteDAO(String usernameSelezionato, ArrayList<Integer> idFotoAssociato) throws SQLException;
 
     // Metodo per ottenere i video associati a un utente.
-    public boolean VediVideoPerUtenteDAO(String usernameSelezionato, ArrayList<int> idVideoAssociato) throws SQLException;
+    public boolean VediVideoPerUtenteDAO(String usernameSelezionato, ArrayList<Integer> idVideoAssociato) throws SQLException;
 
     // Metodo per modificare le informazioni di un utente esistente.
     public boolean modificaUtenteDAO(String usernameSelezionato, String password, boolean admin) throws SQLException;
