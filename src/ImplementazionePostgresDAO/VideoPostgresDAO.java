b3f7466215e5d258bba1fcac8b575e0fb097d981
999en <125801405+999en@users.noreply.github.com>
2023-09-05 17:12:20 +0200

    Update VideoPostgresDAO.java

--- src/ImplementazionePostgresDAO/VideoPostgresDAO.java
+++ src/ImplementazionePostgresDAO/VideoPostgresDAO.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Timestamp;
+import java.sql.Timestamp;
 
 public class VideoPostgresDAO implements VideoDAO{
     private Connection connection;
